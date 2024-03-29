d9d3c0a125806739cd94ea931eb2c63c3befffab
sabaMumladze <sabamumladze7@gmail.com>
2024-03-30 03:29:17 +0400

    wine
    web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ContentComponent } from "./content/content.component";
+import { FooterComponent } from "./footer/footer.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent, FooterComponent]
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  ngOnInit(): void {
+    // alert('WELCOME TO STOFBERG')
+  }
   title = 'wine_web';
+
 }
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,45 @@
+import { style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent implements OnInit {
+// old() {
+// document.querySelector('.image')?.innerHTML =
+// }
+  ngOnInit(): void {
+     // throw new Error('Method not implemented.');
+  //   const IMAGE_WIDTH = 500;
+  //   const DYLAY = 3000;
+  //   const $prev = document.querySelector('.prev');
+  //   const $next = document.querySelector('.next')
+     
+  //   const $imageConteiner = document.querySelector('image_conteiner')
+  //   const $images = document.querySelectorAll('.img')
+  //   let currentImg = 1;
+  //   let timeout; 
+
+
+  //   function updateImg() {
+  //     if(currentImg > $images.length) {
+  //       currentImg = 1;
+  //     }else if (currentImg < 1) {
+  //       currentImg = $images.length
+  //     }
+  //     $imageConteiner.style.transform = ''
+
+  // }
+
+
+    }
+
+  
+
+  
+
+}
